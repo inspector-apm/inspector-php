3738621c3653e2cfa0e24ab6e102fb5e9e123a55
Valerio <ilvalerione@gmail.com>
2019-04-19 12:19:26 +0200

    add transaction field

--- src/LogEngine.php
+++ src/LogEngine.php
@@ -206,12 +206,32 @@ class LogEngine extends AbstractLogger
     }
 
     /**
-     * generate unique ID for grouping events.
+     * Generate unique ID for grouping events.
+     *
+     * http://www.php.net/manual/en/function.uniqid.php#94959
      *
      * @return string
      */
     public function generateTransactionId()
     {
-        return uniqid();
+        return sprintf(
+            '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            // 32 bits for "time_low"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            // 16 bits for "time_mid"
+            mt_rand(0, 0xffff),
+            // 16 bits for "time_hi_and_version",
+            // four most significant bits holds version number 4
+            mt_rand(0, 0x0fff) | 0x4000,
+            // 16 bits, 8 bits for "clk_seq_hi_res",
+            // 8 bits for "clk_seq_low",
+            // two most significant bits holds zero and one for variant DCE1.1
+            mt_rand(0, 0x3fff) | 0x8000,
+            // 48 bits for "node"
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff)
+        );
     }
 }
