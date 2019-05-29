a3c1ba8f0ad2ac6665532c57d5f48a7c98347a27
Valerio <ilvalerione@gmail.com>
2019-05-29 17:27:53 +0200

    fix configuration fluent api

--- tests/Utils.php
+++ tests/Utils.php
@@ -5,6 +5,13 @@ namespace LogEngine\Tests;
 
 class Utils
 {
+    /**
+     * Determine if the given string start with a particular substring.
+     *
+     * @param $haystack
+     * @param $needle
+     * @return bool
+     */
     public static function startsWith($haystack, $needle)
     {
         return substr($haystack, 0, strlen($needle)) === $needle;
