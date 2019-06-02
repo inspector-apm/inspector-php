252b81e6f2f0ea9ca92a2f1e23620a439512d43b
Valerio <ilvalerione@gmail.com>
2019-06-02 12:27:35 +0200

    change exception name

--- src/Models/Context/Request.php
+++ src/Models/Context/Request.php
@@ -65,7 +65,9 @@ class Request extends AbstractContext
         $this->socket = new Socket();
         $this->method = $_SERVER['REQUEST_METHOD'] ?? 'cli';
         $this->httpVersion = substr($_SERVER['SERVER_PROTOCOL'], strpos($_SERVER['SERVER_PROTOCOL'], '/'));
-        $this->headers = getallheaders();
+        if (function_exists('apache_request_headers')) {
+            $this->headers = apache_request_headers();
+        }
         $this->cookies = $_COOKIE;
     }
 
