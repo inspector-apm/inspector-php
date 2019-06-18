9cee700fbe4fb7f90d42586844d232904ce96c68
Valerio <ilvalerione@gmail.com>
2019-06-18 16:14:20 +0200

    fix memory peak calculation

--- src/Models/Transaction.php
+++ src/Models/Transaction.php
@@ -76,7 +76,7 @@ class Transaction extends AbstractModel
             return $this->memoryPeak;
         }
 
-        return round((memory_get_peak_usage(true)/1024/1024), 2);
+        return round((memory_get_peak_usage()/1024/1024), 2);
     }
 
     public function getHash(): string
