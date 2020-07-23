e84d3fd8a5dfdaf28cecd2aefefd92ab8064ed9a
Valerio Barbera <ilvalerione@gmail.com>
2020-07-23 10:26:40 +0200

    fix

--- src/Models/Arrayable.php
+++ src/Models/Arrayable.php
@@ -162,7 +162,7 @@ abstract class Arrayable implements \ArrayAccess, \JsonSerializable
     {
         return array_filter($this->toArray(), function ($value) {
             // remove NULL, FALSE and Empty Strings and Arrays, but leave values of 0 (zero)
-            return is_array($value) ? empty($value) : strlen($value);
+            return is_array($value) ? !empty($value) : strlen($value);
         });
     }
 
