4789a6ee7c649cec067df306c33715b21a8f280b
Valerio Barbera <ilvalerione@gmail.com>
2021-01-08 20:48:58 +0100

    headers addslashes

--- src/Models/Partials/Request.php
+++ src/Models/Partials/Request.php
@@ -22,7 +22,9 @@ class Request extends Arrayable
         $this->cookies = $_COOKIE;
 
         if (function_exists('apache_request_headers')) {
-            $this->headers = apache_request_headers();
+            $this->headers = array_map(function ($value) {
+                return addslashes($value);
+            }, apache_request_headers());
         }
     }
 }
