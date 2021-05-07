332165bd84b841691e49836d9b68b1577493adf5
Valerio Barbera <ilvalerione@gmail.com>
2021-05-07 22:51:06 +0200

    fix server status calc

--- src/Models/Partials/Host.php
+++ src/Models/Partials/Host.php
@@ -28,18 +28,17 @@ class Host extends Arrayable
     {
         if (OS::isLinux() && function_exists('shell_exec')) {
             try {
-                $status = shell_exec('`LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)* id.*/\1/" | awk \'{print 100 - $1}\'`;`free -m | awk \'/Mem:/ { printf("%3.1f%", $3/$2*100) }\'`;`df -h / | awk \'/\// {print $(NF-1)}\'`');
-                var_dump($status);
+                $status = shell_exec('`LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk \'{print 100 - $1}\'`%;`free -m | awk \'/Mem:/ { printf("%3.1f%%", $3/$2*100) }\'`;`df -h / | awk \'/\// {print $(NF-1)}\'`');
                 $status = str_replace('%', '', $status);
                 $status = str_replace("\n", '', $status);
 
-                $status = array_map(function ($item) {
+                /*$status = array_map(function ($item) {
                     return floatval($item);
                 }, explode(';', $status));
 
                 $this->cpu = $status[0];
                 $this->ram = $status[1];
-                $this->hdd = $status[2];
+                $this->hdd = $status[2];*/
             } catch (\Throwable $exception) {
                 // do nothing
             }
