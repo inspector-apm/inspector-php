1f3fbbd7c9942f33f619b0d16f4a767a9f9b9eae
Valerio Barbera <valerio@inspector.dev>
2025-06-18 11:03:45 +0200

    update version

--- src/Configuration.php
+++ src/Configuration.php
@@ -20,7 +20,7 @@ class Configuration
 
     protected string $transport = 'async';
 
-    protected string $version = '3.15.0';
+    protected string $version = '3.15.4';
 
     /**
      * General-purpose options, E.g., we can set the transport proxy.
