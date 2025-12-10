7da3ed54da70cde01daa1476ba089e4cd27b7a24
Valerio Barbera <valerio@inspector.dev>
2025-12-10 12:54:57 +0100

    check json consistency on transport send data

--- src/Configuration.php
+++ src/Configuration.php
@@ -30,7 +30,7 @@ class Configuration
 
     protected string $transport = 'async';
 
-    protected ?string $version = '3.16.1';
+    protected ?string $version = '3.16.5';
 
     /**
      * General-purpose options, E.g., we can set the transport proxy.
